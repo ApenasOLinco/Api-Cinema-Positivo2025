00f81783277ca37d2b107bc5cc70d018aaca8f23
ApenasOLinco <120327456+ApenasOLinco@users.noreply.github.com>
2025-05-28 17:55:26 -0300

    Fix de alguns erros do commit anterior

--- Cinema-Api/src/Models/DTOs/Get/AtorGetDTO.cs
+++ Cinema-Api/src/Models/DTOs/Get/AtorGetDTO.cs
@@ -3,5 +3,5 @@ namespace Cinema_Api.src.Models.DTOs.Get;
 public record AtorGetDTO(string Nome, DateOnly DataNascimento)
 {
 	public AtorGetDTO()
-		: this("", default, null) { }
+		: this("", default) { }
 }
