3d34bab78d99b7a3614eb088c33a658caead3c68
ApenasOLinco <120327456+ApenasOLinco@users.noreply.github.com>
2025-05-28 17:58:04 -0300

    Criada propriedade FilmesAtores na classe FileGetDTO

--- Cinema-Api/src/Models/DTOs/Get/FilmeGetDTO.cs
+++ Cinema-Api/src/Models/DTOs/Get/FilmeGetDTO.cs
@@ -15,4 +15,6 @@ public class FilmeGetDTO
 	public required List<string> Generos { get; set; }
 
 	public required DiretorGetDTO Diretor { get; set; }
+
+	public required List<PapelAtor> Atores { get; set; }
 }
