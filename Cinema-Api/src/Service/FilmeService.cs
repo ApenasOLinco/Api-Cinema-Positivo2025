b988aab78867c6fa90c31267e7b5c2a9245cc69c
ApenasOLinco <120327456+ApenasOLinco@users.noreply.github.com>
2025-05-29 15:00:37 -0300

    removidos métodos não utilizados de FilmeService

--- Cinema-Api/src/Service/FilmeService.cs
+++ Cinema-Api/src/Service/FilmeService.cs
@@ -42,51 +42,6 @@ public class FilmeService(
 			: Mapper.Map<Filme, FilmeGetDTO>(filme);
 	}
 
-	public List<FilmeGetDTO> Filtrar(FilmeFilterDTO filtro)
-	{
-		var filmes = FilmesComInclude()
-			.AsEnumerable()
-			.Select(f => Mapper.Map<Filme, FilmeGetDTO>(f));
-
-		if (filtro.Titulo is not null)
-		{
-			filmes = filmes.Where(f =>
-				f.Titulo.Contains(filtro.Titulo, StringComparison.OrdinalIgnoreCase)
-			);
-		}
-
-		if (filtro.AnoLancamento is not null)
-		{
-			filmes = filmes.Where(f => f.AnoLancamento == filtro.AnoLancamento);
-		}
-
-		if (filtro.Sinopse is not null)
-		{
-			filmes = filmes.Where(f =>
-				f.Sinopse.Contains(filtro.Sinopse, StringComparison.OrdinalIgnoreCase)
-			);
-		}
-
-		if (filtro.Generos is not null && filtro.Generos.Count > 0)
-		{
-			foreach (var genero in filtro.Generos)
-			{
-				filmes = filmes.Where(f =>
-					f.Generos.Any(g => g.Equals(genero, StringComparison.OrdinalIgnoreCase))
-				);
-			}
-		}
-
-		if (filtro.Diretor is not null)
-		{
-			filmes = filmes.Where(f =>
-				f.Diretor.Nome.Equals(filtro.Diretor, StringComparison.OrdinalIgnoreCase)
-			);
-		}
-
-		return [.. filmes];
-	}
-
 	public Filme NovoFilme(FilmePostDTO filmeDto)
 	{
 		var filme = AddFilme(filmeDto);
@@ -95,71 +50,6 @@ public class FilmeService(
 		_masterContext.SaveChanges();
 
 		return filme;
-	}
-
-	public FilmeGetDTO ModificarFilme(int id, FilmePatchDTO patchDto)
-	{
-		var filme = _masterContext
-			.Filme.Include(filme => filme.FilmesGeneros)
-			.ThenInclude(fg => fg.Genero)
-			.Include(filme => filme.Diretor)
-			.First(filme => filme.Id == id);
-
-		if (patchDto.Titulo is not null)
-			filme.Titulo = patchDto.Titulo;
-
-		if (patchDto.AnoLancamento is not null)
-			filme.AnoLancamento = patchDto.AnoLancamento.Value;
-
-		if (patchDto.NotaIMDB is not null)
-			filme.NotaIMDB = patchDto.NotaIMDB.Value;
-
-		if (patchDto.Generos is not null)
-		{
-			// Remove os filmesGeneros do objeto filme para substituí-los
-			filme.FilmesGeneros.Clear();
-
-			foreach (var nomeGenero in patchDto.Generos) // Efetivamente Adiciona os novos gêneros ao filme..,
-			{
-				var genero = _generoService.GetExistenteOuCriar(nomeGenero);
-				filme.FilmesGeneros.Add(new() { FilmeId = filme.Id, GeneroId = genero.Id });
-			}
-		}
-
-		if (patchDto.Diretor is null)
-		{
-			_masterContext.SaveChanges();
-			return Mapper.Map<Filme, FilmeGetDTO>(filme); // Não há necessidade de modificar o diretor, então retorna
-		}
-
-		if (patchDto.IsNovoDiretor is null) // Diretor foi fornecido, então IsNovoDiretor deve ser fornecido também
-		{
-			throw new BadHttpRequestException(
-				"Se um diretor for fornecido, é necessário especificar se "
-					+ "ele deve ser interpretado como um diretor novo através da "
-					+ "propriedade \"IsNovoDiretor\" (true ou false)"
-			);
-		}
-
-		if (patchDto.IsNovoDiretor.Value)
-		{
-			// É um novo diretor, então adiciona ele à database
-			var diretor = _diretorService.NovoDiretor(patchDto.Diretor);
-
-			filme.Diretor = diretor;
-		}
-		else
-		{
-			var diretor = _diretorService.SingleByNomeAndDataNasc(
-				patchDto.Diretor.Nome,
-				patchDto.Diretor.DataNasc
-			);
-
-			filme.Diretor = diretor ?? filme.Diretor;
-		}
-
-		_masterContext.SaveChanges();
-		return Mapper.Map<Filme, FilmeGetDTO>(filme);
 	}
 
 	public void DeletarFilme(int id)
