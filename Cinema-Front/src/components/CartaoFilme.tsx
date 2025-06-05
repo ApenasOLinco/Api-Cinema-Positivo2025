8e42c4716419a77c188b4867cb246723ddb4e42f
ApenasOLinco <120327456+ApenasOLinco@users.noreply.github.com>
2025-06-05 15:57:01 -0300

    Criada pasta models, add Home page

---
+++ Cinema-Front/src/components/CartaoFilme.tsx
@@ -0,0 +1,12 @@
+function CartaoFilme() {
+	return <div className="cartao-filme">
+		<div className="filme-poster">
+			<img src={} alt={}/>
+		</div>
+		<div className="filme-info">
+
+		</div>
+	</div>
+}
+
+export default CartaoFilme;
