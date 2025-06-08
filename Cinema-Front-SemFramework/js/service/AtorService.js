62b620558fcf4c2a3b743fc91ec179077a7f0c9d
ApenasOLinco <120327456+ApenasOLinco@users.noreply.github.com>
2025-06-08 14:47:20 -0300

    Typo

--- Cinema-Front-SemFramework/js/service/AtorService.js
+++ Cinema-Front-SemFramework/js/service/AtorService.js
@@ -1,15 +1,15 @@
-import * as requestService from "./requestService.js";
+import * as requestService from "./RequestService.js";
 
 const FETCH_URL = `${requestService.API_URL}/api/v1/Ator`;
 
 export async function todosOsAtores() {
-	const atores = await requestService.get(FETCH_URL);
+	const atores = await requestService._get(FETCH_URL);
 
 	return atores;
 }
 
 export async function umAtor(id) {
-	const ator = await requestService.get(`${FETCH_URL}/${id}`);
+	const ator = await requestService._get(`${FETCH_URL}/${id}`);
 
 	return ator;
 }
